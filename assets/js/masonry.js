6b03885de7302033028281c0c899e612d2f70295
Anh Vu <anhvungoc.21@gmail.com>
2024-03-04 09:50:07 -0600

    Fix masonry layout

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  console.log("init Masonry");
   // Init Masonry
   var $grid = $(".grid").masonry({
     gutter: 10,
@@ -8,5 +9,6 @@ $(document).ready(function () {
   // Layout Masonry after each image loads
   $grid.imagesLoaded().progress(function () {
     $grid.masonry("layout");
+    console.log("DONE MASONRY");
   });
 });
